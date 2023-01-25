485d48682e1f828ed98c8af61f4d263570233f2a
AAASASN <maraenko@gmail.com>
2023-01-25 22:37:21 +0300

    initial commit

---
+++ AngelaYu_IamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AngelaYu_IamRich
+//
+//  Created by Александр Мараенко on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
